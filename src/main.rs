26db7ca33dc805719af70882b1a1fa3fce3202c4
Zoran Zaric <zz@zoranzaric.de>
2021-06-17 20:49:44 +0200

    Implemet HTTP API communication

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use frankenstein::{api_params::ChatId, Api, GetUpdatesParams, SendMessageParams, TelegramApi};
+use frankenstein::{
+    api_params::ChatId, Api, GetUpdatesParams, Message, SendMessageParams, TelegramApi, Update,
+};
 
 fn main() {
+    let api_url = std::env::var("ORG_HERMES_API_URL").expect("ORG_HERMES_API_URL not set");
     let token = std::env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
 
     let api = Api::new(&token);
@@ -19,18 +22,31 @@ fn main() {
                     if let Some(message) = update.message() {
                         if let Some(text) = message.text.clone() {
                             println!("Text: {}", text);
+
+                            match ureq::post(&api_url).send_json(ureq::json!({
+                                "content": &text,
+                            })) {
+                                Ok(_) => accept_message(
+                                    &mut update_params,
+                                    &api,
+                                    &message,
+                                    update,
+                                    "Thanks for your message!  I took note of it.".into(),
+                                ),
+                                Err(e) => {
+                                    eprintln!("org-hermes-telegram: Error while sending a capture to the api: {}", e);
+                                    accept_message(
+                                        &mut update_params,
+                                        &api,
+                                        &message,
+                                        update,
+                                        "Thanks for your message!  Unfortunatly I could not take note of it.  I won't try again, please make sure to try again later.".into(),
+                                    )
+                                }
+                            };
                         } else {
                             println!("Unhandled message type");
-
-                            let mut send_message_params = SendMessageParams::new(
-                                ChatId::Integer(message.chat().id()),
-                                "Thanks, I received your message!".to_string(),
-                            );
-                            send_message_params.set_reply_to_message_id(Some(message.message_id()));
-
-                            let _ = api.send_message(&send_message_params);
-
-                            update_params.set_offset(Some(update.update_id() + 1))
+                            accept_message(&mut update_params, &api, &message, update, "Thanks for your message! Unfortunatly I am not able to handle the provide message type".into());
                         }
                     }
                 }
@@ -40,4 +56,20 @@ fn main() {
             }
         }
     }
+}
+
+fn accept_message(
+    update_params: &mut GetUpdatesParams,
+    api: &Api,
+    message: &Message,
+    update: Update,
+    response_text: String,
+) {
+    let mut send_message_params =
+        SendMessageParams::new(ChatId::Integer(message.chat().id()), response_text);
+    send_message_params.set_reply_to_message_id(Some(message.message_id()));
+
+    update_params.set_offset(Some(update.update_id() + 1));
+
+    let _ = api.send_message(&send_message_params);
 }
